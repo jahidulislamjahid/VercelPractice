1fceb914503494145456af9c6382e322d50c28d8
jahid <info.jahidulislamjahid@gmail.com>
2022-06-01 01:21:47 +0600

    update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/", async (req, res) => {
 
 // CONNECTION URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtoag.mongodb.net/timetracer?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
